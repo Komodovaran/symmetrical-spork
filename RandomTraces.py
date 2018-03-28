e7f71ebd7721dfbf5eaff8854c9202fa56720322
simonbonano <simonbo92@gmail.com>
2018-03-28 19:28:15 +0200

    Generates random FRET trajectories NB. Transitions between all states are allowed (including self-transitions)

--- RandomTraces.py
+++ RandomTraces.py
@@ -2,120 +2,112 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-
-
-plot_traces = True
-plot_stats  = True
-add_noise   = True
+import os
 
 
 np.random.seed(1)
 
-#####################################
-# Set parameters
-#####################################
+def fret_trace_generator(n_traces            = 50,
+                        true_states          = [0.3, 0.5, 0.7, 0.9],  # True underlying states
+                        true_occupancies     = [0.2, 0.3, 0.4, 0.1],  # True occupancies (must sum up to one)
+                        bleach_time          = 20,                    # Average bleaching time of donor/acceptor
+                        recording_time       = 60,                    # Total recording time
+                        energy_barrier       = 5,                     # Scales the dwell time in each state
+                        noise_level          = 0.05,                  # Noise level
+                        plot_traces          = True,
+                        plot_bleaching_times = True):                # NB. exports to sub folder named "stats"
 
-n_traces         = 50
+    df_traces = pd.DataFrame()
+    bleaching_time_lst = []
 
-true_states      = [0.3, 0.5, 0.7, 0.9]  # True underlying states
-true_occupancies = [0.2, 0.3, 0.4, 0.1]  # True occupancies (must sum up to one)
+    for trace in tqdm(range(n_traces)):
 
-bleach_time      = 20                    # Average bleaching time of donor/acceptor
-recording_time   = 60                    # Total recording time
-energy_barrier   = 5                    # Scales the dwell time in each state
+        # Reset frame and lists
+        frame           = 0
+        id_lst          = []
+        frame_lst       = []
+        fret_lst        = []
+        true_fret_lst   = []
 
-noise_level      = 0.05                 # Noise level
+        # Decide bleaching time
+        bleaching_time = float(np.random.exponential(scale=bleach_time))
 
-#####################################
-# Generate traces
-#####################################
+        if bleaching_time > recording_time:
+            bleaching_time = recording_time
 
-df_traces = pd.DataFrame()
-bleaching_time_lst = []
+        while frame < bleaching_time:
 
-for i in tqdm(range(n_traces)):
+            # Decide state
+            fret = np.random.choice(a=true_states, p=true_occupancies)    # NB. All transitions are are allowed (including self-transitions)
 
-    # Reset frame and lists
-    frame     = 0
-    frame_lst = []
-    fret_lst  = []
-    id_lst = []
+            # Decide dwell time
+            true_occupancy   = true_occupancies[true_states.index(fret)]
+            dwell_time = energy_barrier * float(np.random.exponential(scale=true_occupancy))
 
-    # Decide bleaching time
-    bleaching_time = float(np.random.exponential(scale=bleach_time, size=1))
-
-    if bleaching_time > recording_time:
-        bleaching_time = recording_time
-
-    while frame < bleaching_time:
-
-        # Decide state
-        fret = np.random.choice(a=true_states, p=true_occupancies)    # NB all transitions are are allowed (including self-transitions)
-
-        # Decide dwell time
-        occupancy = true_occupancies[true_states.index(fret)]
-        dwell_time = energy_barrier * float(np.random.exponential(scale=occupancy, size=1))
-
-        for n in range(int(dwell_time)):
-            if add_noise:
+            for n in range(int(dwell_time)):
                 # Generate random noise
                 noise = np.random.normal(loc=0, scale=noise_level)
-            else:
-                noise = 0
 
-            frame_lst.append(frame)
-            fret_lst.append(fret + noise)
-            id_lst.append(i)
+                frame_lst.append(frame)
+                fret_lst.append(fret + noise)
+                true_fret_lst.append(fret)
+                id_lst.append(trace)
 
-            frame +=1
+                frame +=1
 
-    while frame < recording_time:
-        if add_noise:
+        while frame < recording_time:
             # Generate random noise
             noise = np.random.normal(loc=0, scale=noise_level)
-        else:
-            noise = 0
 
-        frame_lst.append(frame)
-        fret_lst.append(noise)
-        id_lst.append(i)
+            frame_lst.append(frame)
+            fret_lst.append(noise)
+            true_fret_lst.append(0)
+            id_lst.append(trace)
 
-        frame += 1
+            frame += 1
 
-    # Append fret values to data frame
-    data_tuples = list(zip(id_lst, frame_lst, fret_lst))
-    df_temp = pd.DataFrame(data_tuples, columns=["ID", "frame", "FRET value"])
+        # Append fret values to data frame
+        data_tuples = list(zip(id_lst, frame_lst, fret_lst, true_fret_lst))
+        df_temp = pd.DataFrame(data_tuples, columns=["ID", "Frame", "FRET (observed)", "FRET (true)"])
 
-    df_traces = pd.concat([df_traces, df_temp], ignore_index=True)
+        df_traces = pd.concat([df_traces, df_temp], ignore_index=True)
 
-    # Append bleaching time to list
-    bleaching_time_lst.append(bleaching_time)
+        # Append bleaching time to list
+        bleaching_time_lst.append(bleaching_time)
 
-    # Plot trace
-    if plot_traces:
-        plt.figure(figsize = (6, 3))
-        plt.plot(frame_lst, fret_lst)
-        plt.xlabel("Frame")
-        plt.ylabel("FRET")
-        plt.xlim(0, recording_time)
-        plt.ylim(-0.1, 1.1)
-        plt.tight_layout()
-        plt.savefig("traces/random_trace_{:d}.pdf".format(i))
-        plt.close()
+        # Plot traces
+        if plot_traces:
+            os.makedirs("random_traces", exist_ok=True)
 
-# Export traces to csv file
-df_traces.to_csv("traces/traces.csv")
+            plt.figure(figsize=(6, 3))
+            plt.plot(frame_lst, fret_lst, label="Observed")
+            plt.plot(frame_lst, true_fret_lst, linestyle="--", linewidth=0.8, color="firebrick", label="True")
+            plt.xlabel("Frame")
+            plt.ylabel("FRET")
+            plt.xlim(0, recording_time)
+            plt.ylim(-0.1, 1.1)
+            plt.legend()
+            plt.tight_layout()
+            plt.savefig("random_traces/random_trace_{:d}.pdf".format(trace))
+            plt.close()
+
+    # Export traces to csv file
+    os.makedirs("random_traces", exist_ok=True)
+
+    df_traces.to_csv("random_traces/random_traces.csv")
+
+    # Distribution of bleaching times
+    if plot_bleaching_times:
+        os.makedirs("random_traces", exist_ok=True)
+
+        plt.figure()
+        plt.hist(bleaching_time_lst, bins=15, label="N_traces = {:d}".format(len(bleaching_time_lst)))
+        plt.xlabel("Bleaching time (frames)")
+        plt.ylabel("#")
+        plt.legend()
+        plt.savefig("random_traces/bleaching_times.pdf")
+
+    return
 
 
-#####################################
-# Plot other stuff
-#####################################
-
-if plot_stats:
-    plt.figure()
-    plt.hist(bleaching_time_lst, bins=15, label="N_traces = {:d}".format(len(bleaching_time_lst)))
-    plt.xlabel("Bleaching time (frames)")
-    plt.ylabel("#")
-    plt.legend()
-    plt.savefig("stats/trace_duration.pdf")
-
+fret_trace_generator(n_traces=50)
